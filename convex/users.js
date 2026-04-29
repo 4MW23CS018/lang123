5d905c2907f43e1069f90b4bf139e67591a13236
Adarsh Devadiga <adarshdevadiga5100@gmail.com>
2026-04-29 08:23:28 +0530

    Revert "first commit"
    This reverts commit caeab8498c95fb4b18c5c9470b3b1aab93de3dd3.

--- convex/users.js
+++
@@ -1,7 +0,0 @@
-import {  query } from "./_generated/server";
-import { v } from "convex/values";
-
-export const get = query({
-  args: { userId: v.id("users") },
-  handler: async (ctx, args) => ctx.db.get(args.userId),
-});
